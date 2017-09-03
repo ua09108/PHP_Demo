8e7aa91668e5c11c8c30ae1421a7eec31626c156
ua09108 <ua09108@hotmail.com>
2017-09-03 21:10:42 +0800

    php

--- 05_string.php
+++ 05_string.php
@@ -12,6 +12,13 @@
 
                 echo "Hello $userName";           //變數的值可以正確被認出
                 echo '<br>Hello $userName';       //變數的值不可以正確被認出
-         ?>
+                echo "<br/>";
+                $Message = <<< MSG
+                    執行者: $userName <br/>
+                    執行時間: 2017/09/03
+MSG;
+
+                echo $Message;
+            ?>
     </body>
 </html>
